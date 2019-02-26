8b380c6719dbf8782c9aef343c5ae3f9107b2b68
Raj <bandoprp@mail.uc.edu>
2019-02-25 21:49:57 -0500

    Update tesla.cpp

--- lab6/tesla.cpp
+++ lab6/tesla.cpp
@@ -5,77 +5,156 @@
 #include <iomanip>
 
 using namespace std;
-bool check(int p){
-  if(p>15 || p<0)
-  cout<<"You cannot move that way! Try again."<<endl;
-  return false;
+
+int
+check (int p)
+{
+  if (p <= 15 || p >= 0)
+    return p;
+  else
+    cout << "You cannot move that way! Try again." << endl;
 }
-int inputx(char a){
-int x=0;
-  if(a=='D'){
-  x+=1;
-  check(x);}
-  else{
-  x-=1;
-check(x);}
-return x;
-  }
-int inputy(char b){
-  int y=0;
-    if(b=='W'){
-    y+=1;
-    check(y);}
-    else{
-    y-=1;
-  check(y);}
-  return y;
+
+int
+elon_is_movingx ()
+{
+  int muskx = rand () % 16 + 1;
+  return muskx;
+}
+
+int
+elon_is_movingy ()
+{
+  int muskY = rand () % 16 + 1;
+  return muskY;
+}
+
+int
+check_if_caught (int a, int b, int xx, int yy)
+{
+  int c = 0;
+  if (a == xx && b == yy)
+    c = c + 1;
+  return c;
+}
+
+int
+main ()
+{
+  string dd = "";
+  bool elon_awake;
+  int a, ttt = 0;
+  int b;
+  int x_elon = 0;
+  int y_elon = 0;
+  int x_yugo = 6;
+  int y_yugo = 8;
+  int x_pinto = 11;
+  int y_pinto = 13;
+  int xt = 9;
+  int yt = 11;
+  int userx = 0;
+  int usery = 0;
+xxqw:
+  cout <<
+    "Press W to move foreword, S to move backward, A to move left and D to move right."
+    << endl;
+  cout <<
+    "Pressing W/A/S/D and pressing return will move you one step in the specified direction."
+    << endl;
+
+here:
+  cout << "Enter your input: ";
+  cin >> dd;
+  if (dd == "W")
+    {
+      userx += 1;
+
     }
-void input(char input){
-  here:
-  
-  if(input=='W' || input=='S')
-  inputy(input);
-  else if (input=='A' || input=='D') {
-  inputx(input);}
-  else{
-  cout<<"Invalid choice entered, try again."<<endl;
-  goto here;}
-  }
+  if (dd == "S")
+    {
+      userx -= 1;
 
+    }
+  if (dd == "D")
+    {
+      usery += 1;
 
- 
-int main(){
- int x_elon=0,a;
- int y_elon=0;
- int x_yugo=6;
- int y_yugo=8;
- int x_pinto=11;
- int y_pinto=13;
- int xt=9;
- int yt=11;
- bool elon_awake=false;
-  cout<<"Press W to move foreword, S to move backward, A to move left and D to move right."<<endl;
-  cout<<"Pressing W/A/S/D and pressing return will move you one step in the specified direction."<<endl;
- 
-  if(rand()%5==3 || rand()%2==1)
-  bool elon_awake=true;
-  if(elon_awake==true){
-    do{
-    cout<<"You see a moving character in distant, be careful!!"<<endl; 
-    x_elon+=rand()%15;
-    y_elon+=rand()%15;
+    }
+  if (dd == "A")
+    {
+      usery -= 1;
 
-    if(input(a)==x_elon && input()==y_elon){
-    c=c+1;
-    if(c>=2)
+    }
+
+  if (rand () % 5 == 3 || rand () % 2 == 1)
+    bool elon_awake = true;
+  if (elon_awake == true)
+    {
+      cout << "You see a moving character in distant, be careful!!" << endl;
+      x_elon = elon_is_movingx ();
+      y_elon = elon_is_movingy ();
+      int ttt = check_if_caught (x_elon, y_elon, userx, usery);
+      if (ttt != 0)
+	{
+	  //    c=0,a,b x_elon=0, y_elon=0,x_yugo=6, y_yugo=8,x_pinto=11,y_pinto=13,xt=9,yt=11;
+	  cout <<
+	    "Elon caught you roaming in unauthorized area!! You lied that you are the Janitor, and you are here to clean his bathroom."
+	    << endl;
+	  cout <<
+	    "He lets you go, and you notice that he checks his phone for your employee records. He will probably know that you lied. Don't get caught again!"
+	    << endl;
+	}
+      if (ttt >= 2)
+	{
+	  cout <<
+	    "He caught you, you are the starman now. Good bye, have a great trip! You LOST!!"
+	    << endl;
+	  goto end;
+	}
+      if (userx == x_yugo && usery == y_yugo)
+	cout <<
+	  "You found a car!! You got in, but bad luck it was Yugo not a Tesla. He caught you, you are the starman now. Good bye, have a great trip! You LOST!!"
+	  << endl;
+      if (userx == x_pinto && usery == y_pinto)
+	cout <<
+	  "You found a car!! You got in, but bad luck it was Pinto not a Tesla. He caught you, you are the starman now. Good bye, have a great trip! You LOST!!"
+	  << endl;
+      if (userx == xt && usery == yt)
+	{
+	  cout << "You found a car!! You got in, and you realize..." << endl;
+	  // delay(900);
+	  cout << "IT WAS TESLA!!! YOU SAVED TESLA!! YOU WON!" << endl;
+	}
+    }
+  else
+    {
+      //   c=0,a,b x_elon=0, y_elon=0,x_yugo=6, y_yugo=8,x_pinto=11,y_pinto=13,xt=9,yt=11;
+      if (userx == x_yugo && usery == y_yugo)
+	cout <<
+	  "You found a car!! You got in, but bad luck it was Yugo not a Tesla. He caught you, you are the starman now. Good bye, have a great trip! You LOST!!"
+	  << endl;
+      if (userx == x_pinto && usery == y_pinto)
+	cout <<
+	  "You found a car!! You got in, but bad luck it was Pinto not a Tesla. He caught you, you are the starman now. Good bye, have a great trip! You LOST!!"
+	  << endl;
+      if (userx == xt && usery == yt)
+	{
+	  cout << "You found a car!! You got in, and you realize..." << endl;
+	  //delay(900);
+	  cout << "IT WAS TESLA!!! YOU SAVED TESLA!! YOU WON!" << endl;
+	}
+
+    }
+
+  cout << "Enter another input? Press E to exit or ant other key to continue"
+    << endl;
+  char h;
+  cin >> h;
+  if (h == 'E')
     goto end;
-    cout<<"Elon caught you roaming in unauthorized area!! You lied that you are the Janitor, and you are here to clean his bathroom."<<endl;
-    cout<<"He lets you go, and you notice that he checks his phone for your employee records. He will probably know that you lied. Don't get caught again!"<<endl;}
-    else if(abs(inputx()-x_elon)<=2 && abs(inputy()-y_elon)<=2)
-    cout<<"Its Elon, you need to hide fast. Don't go near him."<<endl;
-  }
-    while(check(x)!=false && check(y)!=false);
-    
-    
-  }
+  else
+    goto here;
+end:
+  cout << "Game Over!! " << endl;
 }
